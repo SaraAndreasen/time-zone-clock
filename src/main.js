3fb99cffc1efcd370d58ee9d8883f37a51ab7d71
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-06-25 22:56:24 +0200

    add working time with moment.js

--- src/main.js
+++ src/main.js
@@ -11,3 +11,24 @@ function selectedCountry(event) {
 
 let countrySelect = document.querySelector("#cities");
 countrySelect.addEventListener("change", selectedCountry);
+
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment.tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let osloElement = document.querySelector("#oslo");
+  let osloDateElement = osloElement.querySelector(".date");
+  let osloTimeElement = osloElement.querySelector(".time");
+  let osloTime = moment.tz("Europe/Oslo");
+  osloDateElement.innerHTML = osloTime.format("MMMM Do, YYYY");
+  osloTimeElement.innerHTML = osloTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);

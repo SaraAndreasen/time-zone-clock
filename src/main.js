35a1b4b62b2eac3204ff7f8b37ae191285e3bb79
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-06-25 20:29:55 +0200

    initial styling, revamp JS, add footer and other html elements

--- src/main.js
+++ src/main.js
@@ -1,23 +1,11 @@
 // Alert the current date and time of the selected city with the timezone
 function selectedCountry(event) {
-  console.log(event.target.value);
-  if (event.target.value === "paris") {
-    let currentTime = moment
-      .tz("Europe/Paris")
-      .format("[It is] dddd, D MMMM, YYYY, hh:mm A [in Europe/Paris]");
-    alert(currentTime);
-  }
-  if (event.target.value === "tokyo") {
-    let currentTime = moment
-      .tz("Asia/Tokyo")
-      .format("[It is] dddd, D MMMM, YYYY, hh:mm A [in Asia/Tokyo]");
-    alert(currentTime);
-  }
-  if (event.target.value === "sydney") {
-    let currentTime = moment
-      .tz("Australia/Sydney")
-      .format("[It is] dddd, D MMMM, YYYY, hh:mm A [in Australia/Sydney]");
-    alert(currentTime);
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
+
+    alert(`It is ${currentTime} in ${event.target.value}`);
   }
 }
 

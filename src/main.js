e3d915fb23ff052840df43495968715695e75f18
SaraAndreasen <55578265+SaraAndreasen@users.noreply.github.com>
2024-06-25 23:21:35 +0200

    add time to multiple hardcoded cities

--- src/main.js
+++ src/main.js
@@ -1,34 +1,44 @@
-// Alert the current date and time of the selected city with the timezone
-function selectedCountry(event) {
-  if (event.target.value.length > 0) {
-    let currentTime = moment()
-      .tz(event.target.value)
-      .format("dddd, MMMM D, YYYY h:mm A");
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment.tz("America/Los_Angeles");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-    alert(`It is ${currentTime} in ${event.target.value}`);
+  let osloElement = document.querySelector("#oslo");
+  if (osloElement) {
+    let osloDateElement = osloElement.querySelector(".date");
+    let osloTimeElement = osloElement.querySelector(".time");
+    let osloTime = moment.tz("Europe/Oslo");
+    osloDateElement.innerHTML = osloTime.format("MMMM Do, YYYY");
+    osloTimeElement.innerHTML = osloTime.format("h:mm:ss [<small>]A[</small>]");
   }
 }
 
-let countrySelect = document.querySelector("#cities");
-countrySelect.addEventListener("change", selectedCountry);
-
-function updateTime() {
-  let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment.tz("America/Los_Angeles");
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do, YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-
-  let osloElement = document.querySelector("#oslo");
-  let osloDateElement = osloElement.querySelector(".date");
-  let osloTimeElement = osloElement.querySelector(".time");
-  let osloTime = moment.tz("Europe/Oslo");
-  osloDateElement.innerHTML = osloTime.format("MMMM Do, YYYY");
-  osloTimeElement.innerHTML = osloTime.format("h:mm:ss [<small>]A[</small>]");
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.replace("_", "").split("/")[1];
+  let cityTime = moment.tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+    </div>
+    <div class="time">
+    ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>
+    </div>
+  </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectorElement = document.querySelector("#city-selector");
+citiesSelectorElement.addEventListener("change", updateCity);
